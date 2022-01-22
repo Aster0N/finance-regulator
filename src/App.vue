8db28b94ae428b8a7de921dc3db838981adb7133
Aster0N <nikvas131@gmail.com>
2022-01-22 18:33:28 +0300

    Initial files setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
 	<div id="app">
-		init
+		<div class="page-title">finance regulator</div>
+
+		<main>
+			<div class="container">
+				<div class="workflow">
+					<div class="workflow-user-input"></div>
+					<div class="workflow-output"></div>
+				</div>
+				<div class="regulator-core">
+					<form class="finance-regulator-form" action="">
+						<label class="form-label">income:</label>
+						<input type="text" class="form-field" />
+					</form>
+					<div class="regulator-output"></div>
+				</div>
+			</div>
+		</main>
 	</div>
 </template>
 
@@ -10,6 +26,24 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+@import url("./assets/scss/style.scss");
+@import url("./assets/scss/variables.scss");
+// font-family: 'Palanquin', sans-serif;
+// font-family: 'Playfair Display SC', serif;
+main {
+	font-family: "Palanquin", sans-serif;
+	font-size: 20px;
+	font-weight: regular;
+}
+.page-title {
+	width: 100%;
+	padding: 150px 0 0 0;
+	text-align: center;
+	font-family: "Playfair Display SC", serif;
+	font-size: 40px;
+	font-weight: regular;
+	text-transform: uppercase;
+	letter-spacing: 25px;
+}
 </style>
